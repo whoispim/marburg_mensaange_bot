aab6f6fcfe8c7a2faf18dbf6d40669fc14143dda
whoispim <pimbot@bk.ru>
2017-10-23 13:10:49 +0200

    simplified through function and finally looked up how to .lower()

--- istdaseinbot.py
+++ istdaseinbot.py
@@ -14,70 +14,46 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Es ist angerichtet!")
 
 def mensa(bot, update):
-    raw, err = subprocess.Popen(['./oneliner.sh'], stdout=subprocess.PIPE).communicate()
-    
-    blue = raw.decode("utf-8")
-    
-    menuint = 0
-    rare = []
-    rare.append("")
-    
-    out = ""
-
-    for line in blue.splitlines():
-        if line != " ":
-            rare[menuint] = rare[menuint] + line + " "
-        else:
-            menuint += 1
-            rare.append("")
-    
-    rare = rare[:-1]
-    
-    boese = ['12','13','17','18','19','22','29','Rind','rind','Pute','pute','Huhn','huhn','Hähn','hähn','geflügel','Geflügel','chicken','Chicken']
-    
-    for gericht in rare:
-        if any (x in gericht for x in boese):
-            out = out + "💔 " + gericht + "\n"
-        else:
-            out = out + "💚 " + gericht + "\n"
+    out = essen_fassen();
     if out == "":
         out = "¯\_(ツ)_/¯"
     bot.send_message(chat_id=update.message.chat_id, text=out)
 
-
-
 def ohnebeilagen(bot, update):
-    raw, err = subprocess.Popen(['./oneliner.sh'], stdout=subprocess.PIPE).communicate()
-    
-    blue = raw.decode("utf-8")
-    
-    menuint = 0
-    rare = []
-    rare.append("")
-    
-    out = ""
-
-    for line in blue.splitlines():
-        if line != " ":
-            rare[menuint] = rare[menuint] + line + " "
-        else:
-            menuint += 1
-            rare.append("")
-    
-    rare = rare[:-1]
-    
-    boese = ['12','13','17','18','19','22','29','Rind','rind','Pute','pute','Huhn','huhn','Hähn','hähn','geflügel','Geflügel','chicken','Chicken']
-    
-    for gericht in rare:
-        if any (x in gericht for x in boese):
-            out = out + "💔 " + gericht + "\n"
-        else:
-            out = out + "💚 " + gericht + "\n"
+    out = essen_fassen();
     out = re.sub(r' \(.+?\)','',out)
     if out == "":
         out = "¯\_(ツ)_/¯"
     bot.send_message(chat_id=update.message.chat_id, text=out)
 
+def essen_fassen(): #fetches menu through oneliner.sh, seperates it and examines for veganisch
+    raw, err = subprocess.Popen(['./oneliner.sh'], stdout=subprocess.PIPE).communicate()
+
+    blue = raw.decode("utf-8")
+
+    menuint = 0
+    rare = []
+    rare.append("")
+
+    out = ""
+
+    for line in blue.splitlines():
+        if line != " ":
+            rare[menuint] = rare[menuint] + line + " "
+        else:
+            menuint += 1
+            rare.append("")
+
+    rare = rare[:-1]
+
+    boese = ['12','13','17','18','19','22','29','rind','pute','huhn','hähn','geflügel','chicken']
+
+    for gericht in rare:
+        if any (x in gericht.lower() for x in boese):
+            out = out + "💔 " + gericht + "\n"
+        else:
+            out = out + "💚 " + gericht + "\n"
+    return out 
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
